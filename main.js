83443ff7a8e69684514c15bdd57582c7f8b53402
danding <33542476+tangchunwei@users.noreply.github.com>
2021-09-06 15:02:06 +0800

    Update main.js
    #换了一个接口API
    #优化部分代码

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
 device.wakeUp();
 //获取今天的日期
-let date  = new Date();
-date.yyyymmdd();
-log("今日时间：",date.yyyymmdd());
+let today = getNowFormatDate();
 //获取今天工作日还是节假日
 let dayInfo = getDayInfo();
-log("今天日期类型：",dayInfo.info);
-// 获取大小周
-// let bsweek = getYearWeek(today);
-log("本周六是否上班：",bsweek);
+log("今天日期类型(0 工作日 1 假日 2 节假日)：",dayInfo.type);
+log("今日时间：",dayInfo.date);
 //随机延时10秒
-sleep(random(0, 10*1000));
-if(dayInfo.info == '工作日')
+sleep(random(0, 8*1000));
+//工作日
+if(dayInfo.type == 0)
 {
     //打开企业微信，解锁屏幕，停留一分钟，回到主页，一键锁屏
     log('今天是工作日，执行打卡操作');
     app.launchApp("企业微信");
     swipe(500,2000,500,1000,201);
-    sleep(1*60*1000);
+    sleep(1*20*1000);
     home();
     app.launchApp("一键锁屏");
 }
-else if(dayInfo.info == '双休日')
+// 双休日
+else if(dayInfo.type == 1)
 {
-    if(dayInfo.cnweekday == '星期六' && bsweek == '这周上班')
+    //周六，且是大周
+    if(dayInfo.weekDay == 6 && dayInfo.weekOfYear%2 == 0)
     {
         log('今天是双休日，但是周六上班，执行打卡操作');
         //打开企业微信，解锁屏幕，停留一分钟，回到主页，一键锁屏
         app.launchApp("企业微信");
         swipe(500,2000,500,1000,201);
-        sleep(1*60*1000);
+        sleep(1*20*1000);
         home();
         app.launchApp("一键锁屏");
     }
@@ -38,33 +37,44 @@ else if(dayInfo.info == '双休日')
         log('双休日，不执行打卡操作');
     }
 }
+//节假日
+else if(dayInfo.type == 2)
+{
+    log('节假日，不执行打卡操作');
+}
 
 
-// 获取当日信息
+// 获取当日节假日信息
 function getDayInfo() {
-    let jjrUrl = "https://api.tianapi.com/txapi/jiejiari/index?key=90f0735f4b6189789328aefb88bf2e03&date="+today;
-    var r = http.get("www.baidu.com", {
+    // 日期模拟
+    let jjrUrl = "https://www.mxnzp.com/api/holiday/single/20211030";
+    // let jjrUrl = "https://www.mxnzp.com/api/holiday/single/"+today;
+    var res = http.get(jjrUrl, {
         headers: {
-            'Accept-Language': 'zh-cn,zh;q=0.5',
-            'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11'
+            'app_id': 'wqwxtfk1kqsxlllp',
+            'app_secret': 'V0tSZW5DdzZhNUM3TEV5bVRFUDlNUT09'
         }
-    });
-    
-    let res = http.get(jjrUrl);
+    });    
     if(res.statusCode != 200){
-        toast("请求失败: " + res.statusCode + " " + res.statusMessage);
+        toast("请求API失败: " + res.statusCode + " " + res.statusMessage);
     }else{
         let data = res.body.json();
-        return data.newslist[0];
+        return data.data;
     }
 }
 
-Date.prototype.yyyymmdd = function() {
-    var mm = this.getMonth() + 1; // getMonth() is zero-based
-    var dd = this.getDate();
-  
-    return [this.getFullYear(),
-            (mm>9 ? '' : '0') + mm,
-            (dd>9 ? '' : '0') + dd
-           ].join('');
-  };
+//获取当日时间
+function getNowFormatDate() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = '0' + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = '0' + strDate;
+    }
+    let currentdate = year   + month  + strDate;
+    return currentdate;
+}
